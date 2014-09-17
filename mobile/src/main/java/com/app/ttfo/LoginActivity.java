4036664d74dd04f0b69be5768c6bd1662c283e5b
leonardykris <alendory@gmail.com>
2014-09-18 01:56:18 +0800

    1. Login page UI finished. Login, FB Login and Sign Up button provided 2. Included button to access LoginActivity from main class. Title = Test Login 3. Updated ttfo-logo.png, previous version has its O cut off at the bottom 4. Updated password hint font to mimic default 5. Changed Login and FB Login to 115dp each

--- mobile/src/main/java/com/app/ttfo/LoginActivity.java
+++ mobile/src/main/java/com/app/ttfo/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.app.ttfo;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
+import android.widget.EditText;
 
 /**
  * Created by kevintanhongann on 9/17/14.
@@ -13,5 +16,10 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        EditText password = (EditText) findViewById(R.id.editText2);
+        password.setTypeface(Typeface.DEFAULT);
+        password.setTransformationMethod(new PasswordTransformationMethod());
     }
+
 }
--- mobile/src/main/java/com/app/ttfo/MainActivity.java
+++ mobile/src/main/java/com/app/ttfo/MainActivity.java
@@ -1,9 +1,12 @@
 package com.app.ttfo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
@@ -12,6 +15,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
 
@@ -37,5 +41,10 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void toLogin(View view){
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 }

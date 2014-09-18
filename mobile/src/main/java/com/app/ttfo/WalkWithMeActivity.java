1be1698eeecb1502533176393493b3f232c61a67
Kevin Tan Hong Ann <kevintanhongann@gmail.com>
2014-09-18 13:10:09 +0800

    lock screen tweak

--- mobile/src/main/java/com/app/ttfo/WalkWithMeActivity.java
+++ mobile/src/main/java/com/app/ttfo/WalkWithMeActivity.java
@@ -1,17 +1,60 @@
 package com.app.ttfo;
 
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+
+import butterknife.ButterKnife;
 
 /**
  * Created by kevintanhongann on 9/17/14.
  */
-public class WalkWithMeActivity extends Activity {
+public class WalkWithMeActivity extends Activity implements SensorEventListener {
+
+
+    private SensorManager mSensorManager;
+
+    private Sensor mStepCounterSensor;
+
+    private Sensor mStepDetectorSensor;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_walkwithme);
+        ButterKnife.inject(this);
+
+        mSensorManager = (SensorManager)
+                getSystemService(Context.SENSOR_SERVICE);
+        mStepCounterSensor = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        mStepDetectorSensor = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mStepCounterSensor,
+                SensorManager.SENSOR_DELAY_FASTEST);
+        mSensorManager.registerListener(this, mStepDetectorSensor,
+                SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }

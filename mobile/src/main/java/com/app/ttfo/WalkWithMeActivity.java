c763a92dd7ac8bdf71c2f870e830a8977859a145
Kevin Tan Hong Ann <kevintanhongann@gmail.com>
2014-09-18 13:57:47 +0800

    dim brightness while walking

--- mobile/src/main/java/com/app/ttfo/WalkWithMeActivity.java
+++ mobile/src/main/java/com/app/ttfo/WalkWithMeActivity.java
@@ -7,8 +7,17 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * Created by kevintanhongann on 9/17/14.
@@ -21,6 +30,9 @@ public class WalkWithMeActivity extends Activity implements SensorEventListener 
     private Sensor mStepCounterSensor;
 
     private Sensor mStepDetectorSensor;
+
+    private float curBrightnessValue;
+    private Sensor mAccelerometerSensor;
 
 
     @Override
@@ -36,21 +48,70 @@ public class WalkWithMeActivity extends Activity implements SensorEventListener 
         mStepDetectorSensor = mSensorManager
                 .getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
 
+        mAccelerometerSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
+        try {
+            curBrightnessValue = android.provider.Settings.System.getInt(
+                    getContentResolver(), android.provider.Settings.System.SCREEN_BRIGHTNESS);
+        } catch (Settings.SettingNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        mSensorManager.registerListener(this, mStepCounterSensor,
+                SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mStepDetectorSensor,
+                SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mAccelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mSensorManager.unregisterListener(this, mStepCounterSensor);
+        mSensorManager.unregisterListener(this, mStepDetectorSensor);
+        mSensorManager.unregisterListener(this, mAccelerometerSensor);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mSensorManager.registerListener(this, mStepCounterSensor,
-                SensorManager.SENSOR_DELAY_FASTEST);
-        mSensorManager.registerListener(this, mStepDetectorSensor,
-                SensorManager.SENSOR_DELAY_FASTEST);
+
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
+        Sensor sensor = event.sensor;
+        float[] values = event.values;
+        int value = -1;
 
+        if (values.length > 0) {
+            value = (int) values[0];
+        }
+
+        if (sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
+            //textView.setText("Step Counter Detected : " + value);
+        } else if (sensor.getType() == Sensor.TYPE_STEP_DETECTOR) {
+            // For test only. Only allowed value is 1.0 i.e. for step taken
+            //textView.setText("Step Detector Detected : " + value);
+
+
+        }
+
+        final WindowManager.LayoutParams lp = getWindow().getAttributes();
+        if (value > 0) {
+            final int finalValue = value;
+            new Timer().schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    lp.screenBrightness = 1 / 100.0f;
+                    Log.d("walkwithme", "value "+ finalValue);
+                }
+            }, 0, 5000);
+
+        } else {
+            lp.screenBrightness = curBrightnessValue;
+        }
+        getWindow().setAttributes(lp);
     }
 
     @Override

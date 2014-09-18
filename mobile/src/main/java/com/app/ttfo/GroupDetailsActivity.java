bd6846fe8ad5dedbc49ec3aa6ca3690c1bd69f65
Kevin Tan Hong Ann <kevintanhongann@gmail.com>
2014-09-18 11:02:34 +0800

    lockscreen wip

--- mobile/src/main/java/com/app/ttfo/GroupDetailsActivity.java
+++ mobile/src/main/java/com/app/ttfo/GroupDetailsActivity.java
@@ -6,6 +6,7 @@ import android.app.admin.DevicePolicyManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
@@ -13,9 +14,11 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.app.ttfo.receiver.MyAdminReceiver;
+import com.michaelnovakjr.numberpicker.NumberPicker;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import de.greenrobot.event.EventBus;
 
 /**
  * Created by kevintanhongann on 9/18/14.
@@ -28,37 +31,30 @@ public class GroupDetailsActivity extends Activity {
     private View.OnClickListener onStartLockClick = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-
             View numberPickerView = getLayoutInflater().inflate(R.layout.layout_numberpickerview, null);
-            AlertDialog dialog = new AlertDialog.Builder(getApplicationContext())
+            final NumberPicker numberPicker = (NumberPicker) numberPickerView.findViewById(R.id.numberpicker);
+            numberPicker.setCurrent(1);
+            AlertDialog dialog = new AlertDialog.Builder(GroupDetailsActivity.this)
                     .setTitle("Set Hours")
                     .setView(numberPickerView)
                     .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
-                            boolean isAdmin = mDevicePolicyManager.isAdminActive(mComponentName);
-                            if (isAdmin) {
-                                mDevicePolicyManager.lockNow();
-                            }else{
-                                Toast.makeText(getApplicationContext(), "Not Registered as admin", Toast.LENGTH_SHORT).show();
-                            }
+
+                            EventBus.getDefault().postSticky(new Integer(numberPicker.getCurrent()));
+                            startActivity(new Intent(GroupDetailsActivity.this, LockScreenActivity.class));
                         }
                     })
                     .create();
             dialog.show();
         }
     };
-    private DevicePolicyManager mDevicePolicyManager;
-    private ComponentName mComponentName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_groupdetails);
         ButterKnife.inject(this);
-        mDevicePolicyManager = (DevicePolicyManager)getSystemService(
-                Context.DEVICE_POLICY_SERVICE);
-        mComponentName = new ComponentName(this, MyAdminReceiver.class);
         startLockBtn.setOnClickListener(onStartLockClick);
     }
 
--- mobile/src/main/java/com/app/ttfo/LockScreenActivity.java
+++ mobile/src/main/java/com/app/ttfo/LockScreenActivity.java
@@ -1,16 +1,81 @@
 package com.app.ttfo;
 
 import android.app.Activity;
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.app.ttfo.receiver.MyAdminReceiver;
+
+import butterknife.InjectView;
+import butterknife.Optional;
+import de.greenrobot.event.EventBus;
 
 /**
  * Created by kevintanhongann on 9/17/14.
  */
 public class LockScreenActivity extends Activity {
 
+    private DevicePolicyManager mDevicePolicyManager;
+    private ComponentName mComponentName;
+    private static final int ADMIN_INTENT = 15;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lockscreen);
+        mDevicePolicyManager = (DevicePolicyManager) getSystemService(
+                Context.DEVICE_POLICY_SERVICE);
+        mComponentName = new ComponentName(this, MyAdminReceiver.class);
+
+        String description = "You need to activate device admin to continue";
+
+        boolean isAdmin = mDevicePolicyManager.isAdminActive(mComponentName);
+        if (isAdmin) {
+            mDevicePolicyManager.lockNow();
+        } else {
+            Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+            intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, mComponentName);
+            intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION,description);
+            startActivityForResult(intent, ADMIN_INTENT);
+            Toast.makeText(getApplicationContext(), "Not Registered as admin", Toast.LENGTH_SHORT).show();
+        }
+
+        Integer hour = EventBus.getDefault().getStickyEvent(Integer.class);
+
+        /*if (hour != null) {
+            new CountDownTimer(hour * 60 * 60 * 1000, 1000) {
+
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    Log.d("timeTillFinish", "timeTillFinish " + millisUntilFinished);
+
+                }
+
+                @Override
+                public void onFinish() {
+                    //disable the lockscreen
+
+                }
+            }.start();
+        }*/
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == ADMIN_INTENT){
+            if(resultCode == Activity.RESULT_OK){
+                mDevicePolicyManager.lockNow();
+            }else{
+                Toast.makeText(getApplicationContext(), "Not Registered as admin", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
